4d86b3592acf7dd7768507d5c0770cf02c374934
sinelnikovd <webxrab@gmail.com>
2016-02-03 14:24:28 +0600

    Главная с скриптами

--- app/js/common.js
+++ app/js/common.js
@@ -4,25 +4,34 @@ $(document).ready(function() {
 		$('input.data-inputmask').mask("+7 (999) 999-99-99");
 	}
 
+	$('select').niceSelect();
 
-	var carouselCustomer = $('.slides').owlCarousel({
+	/*$('.slides__item').each(function(){
+		$('.slides-nav__dots').append('<div class="slides-nav__dot"></div>')
+	});*/
+
+
+
+	var slides = $('.slides').owlCarousel({
 		loop: true,
+		dotsContainer: '.slides-nav__dots',
 		nav: false,
+		dots: true,
 		items: 1
 	})
-	/*$('.carousel-customer__prev').click(function() {
-		carouselCustomer.trigger('prev.owl.carousel');
+	$('.slides-nav__prev').click(function() {
+		slides.trigger('prev.owl.carousel');
 	})
-	$('.carousel-customer__next').click(function() {
-		carouselCustomer.trigger('next.owl.carousel');
-	})*/
+	$('.slides-nav__next').click(function() {
+		slides.trigger('next.owl.carousel');
+	})
 
 	$('.card__slides').owlCarousel({
 		loop: true,
 		nav: false,
+		dots: false,
 		items: 1
 	})
-
 	$('.card__nav-prev').click(function() {
 		$(this).closest('.card').find('.card__slides').trigger('prev.owl.carousel');
 	})
@@ -30,11 +39,93 @@ $(document).ready(function() {
 		$(this).closest('.card').find('.card__slides').trigger('next.owl.carousel');
 	})
 
-	$('.client').owlCarousel({
+
+
+	var client = $('.client').owlCarousel({
 		loop: true,
 		nav: false,
+		dots: false,
 		items: 5
 	})
+	$('.client-slider__prev').click(function() {
+		client.trigger('prev.owl.carousel');
+	})
+	$('.client-slider__next').click(function() {
+		client.trigger('next.owl.carousel');
+	})
+
+	var datein = new Date();
+	datein.setDate((new Date()).getDate()+1);
+	$.datetimepicker.setLocale('ru');
+	$('#datein').datetimepicker({
+		i18n:{
+			ru:{
+				months:[
+					'Январь','Февраль','Март','Апрель',
+					'Май','Июнь','Июль','Август',
+					'Сентябрь','Октябрь','Ноябрь','Декабрь',
+				],
+				dayOfWeek:[
+					"Пн", "Вт", "Ср", "Чт", 
+					"Пт", "Сб", "Вс",
+				]
+			}
+		},
+		timepicker:false,
+		format:'d.m.Y',
+		value: datein
+	});
+
+
+	dateout = new Date();
+	dateout.setDate(datein.getDate()+10);
+	$('#dateout').datetimepicker({
+		i18n:{
+			ru:{
+				months:[
+					'Январь','Февраль','Март','Апрель',
+					'Май','Июнь','Июль','Август',
+					'Сентябрь','Октябрь','Ноябрь','Декабрь',
+				],
+				dayOfWeek:[
+					"Пн", "Вт", "Ср", "Чт", 
+					"Пт", "Сб", "Вс",
+				]
+			}
+		},
+		timepicker:false,
+		format:'d.m.Y',
+		value: dateout
+	});
+
+	var date = new Date(),
+			day = date.getDay(),
+			mounth = date.getMonth(),
+			oldhours = hours = date.getHours(),
+			oldminutes = minutes = date.getMinutes(),
+			months = [
+					'Январь','Февраль','Март','Апрель',
+					'Май','Июнь','Июль','Август',
+					'Сентябрь','Октябрь','Ноябрь','Декабрь',
+				];
+	if (oldhours < 10) hours = "0" + oldhours;
+	if (oldminutes < 10) minutes = "0" + oldminutes;
+	$("#time").text(hours + ":" + minutes);
+	$("#date").text(day + ' ' +months[mounth]);
+
+	setInterval(function () {
+		var date = new Date(),
+				hours = date.getHours(),
+				minutes = date.getMinutes();
+		if (hours < 10) hours = "0" + hours;
+		if (minutes < 10) minutes = "0" + minutes;
+		if( oldminutes < minutes || oldhours < hours){
+			oldminutes = minutes; oldhours = hours;
+			$("#time").text(hours + ":" + minutes);
+		}
+	}, 1000);
+
+
 
 });
 
